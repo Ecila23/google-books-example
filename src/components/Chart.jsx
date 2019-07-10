cf6f3566fc44ea3b18a9c91e9d3b81b1cd17d713
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-10 10:53:54 +0100

    fix Chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -5,34 +5,34 @@ import faker from 'faker';
 const Chart = ({ books }) => {
   if (books.length > 0) {
     const tally = books.reduce((acc, book) => {
+      console.log(book.volumeInfo.categories);
       if (book.volumeInfo.categories) {
         book.volumeInfo.categories.forEach(category => {
           acc[category] = (acc[category] || 0) + 1;
         });
       }
       return acc;
-    });
+    }, {});
     const labels = Object.keys(tally);
     const chartData = labels.map(label => {
       return tally[label];
     });
-    const backgroundColors = labels.map(label => {
-      return faker.commerce.color();
-    });
-
     const data = {
       labels,
-      dataSets: [
+      datasets: [
         {
           data: chartData,
-          backgroundColor: backgroundColors
+          backgroundColor: labels.map(label => {
+            return faker.commerce.color();
+          })
         }
       ]
     };
 
-    return <Pie data={data} />;
+    return <Pie className='chart' data={data} />;
+  } else {
+    return <p className='chart'>No books here!</p>;
   }
-  return <p>No books here!</p>;
 };
 
 export default Chart;
